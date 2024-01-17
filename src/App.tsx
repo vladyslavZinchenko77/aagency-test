69c21c957223f77f730a83e1da9c9609a8fe9049
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-01-17 16:02:38 +0200

    added content on acounts page

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,16 @@
 import { FC } from 'react';
 import Header from './components/Header';
-import TableCustom from './components/TableCustom';
+import Accounts from './pages/Accounts';
 import Footer from './components/Footer';
 
 import './App.scss';
 
-const columns = [
-  {
-    key: 'accountId',
-    label: 'Account ID',
-    render: (item: any) => item.accountId,
-  },
-  {
-    key: 'email',
-    label: 'Email',
-    render: (item: any) => item.email,
-  },
-  {
-    key: 'authToken',
-    label: 'Auth Token',
-    render: (item: any) => item.authToken,
-  },
-  {
-    key: 'creationDate',
-    label: 'Creation Date',
-    render: (item: any) => item.creationDate,
-  },
-];
-
 const App: FC = () => {
-  const initialData = [
-    {
-      accountId: 1,
-      email: 'example1@example.com',
-      authToken: 'token1',
-      creationDate: '2022-01-01',
-    },
-    {
-      accountId: 2,
-      email: 'example2@example.com',
-      authToken: 'token2',
-      creationDate: '2022-01-02',
-    },
-  ];
-
   return (
     <>
       <Header />
       <main>
-        <TableCustom columns={columns} totalItems={20} data={initialData} />
+        <Accounts />
       </main>
       <Footer />
     </>
