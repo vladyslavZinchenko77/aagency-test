ebda70ea173b5ed9b741d96206ee7dcc82a6007f
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-01-17 23:09:21 +0200

    added sorting by cost and clicks on campings page

--- src/pages/Campaigns/Campaigns.tsx
+++ src/pages/Campaigns/Campaigns.tsx
@@ -1,36 +1,3 @@
-// import { FC } from 'react';
-// import { useParams } from 'react-router-dom';
-// import DefaultPage from '../../components/DefaultPage';
-// import TableCustom from '../../components/TableCustom';
-// import { columnsCampaign } from '../../data/columns';
-// import { initialDataCampaings } from '../../data/initialData';
-
-// const Campaigns: FC = () => {
-//   const { id } = useParams();
-
-//   if (id === undefined) {
-//     return <div>Failed to retrieve profile ID</div>;
-//   }
-
-//   const parsedId = parseInt(id, 10);
-
-//   const campaignData = initialDataCampaings.filter(
-//     (campaign) => campaign.profileId === parsedId
-//   );
-
-//   return (
-//     <DefaultPage title={`Campaigns of selected profile ${id}`}>
-//       <TableCustom
-//         columns={columnsCampaign}
-//         data={campaignData}
-//         isClicableRow={false}
-//       />
-//     </DefaultPage>
-//   );
-// };
-
-// export default Campaigns;
-
 import { FC, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import DefaultPage from '../../components/DefaultPage';
@@ -39,6 +6,18 @@ import { columnsCampaign } from '../../data/columns';
 import { initialDataCampaings } from '../../data/initialData';
 import { Button } from 'react-bootstrap';
 import './Campaigns.scss';
+
+type Campaign = {
+  campaignId: number;
+  profileId: number;
+  clicks: number;
+  cost: number;
+  date: string;
+};
+
+type CampaignWithIndex = Campaign & {
+  [key: string]: number | string;
+};
 
 const Campaigns: FC = () => {
   const { id } = useParams();
@@ -49,15 +28,26 @@ const Campaigns: FC = () => {
 
   const parsedId = parseInt(id, 10);
 
-  const [sortedData, setSortedData] = useState(initialDataCampaings);
+  const [sortedData, setSortedData] =
+    useState<CampaignWithIndex[]>(initialDataCampaings);
   const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
 
-  const handleSort = () => {
+  const handleSort = (property: string) => {
     const sorted = [...sortedData].sort((a, b) => {
-      const dateA = new Date(a.date).getTime();
-      const dateB = new Date(b.date).getTime();
+      const valueA = a[property] as number | string;
+      const valueB = b[property] as number | string;
 
-      return sortOrder === 'asc' ? dateA - dateB : dateB - dateA;
+      if (typeof valueA === 'number' && typeof valueB === 'number') {
+        return sortOrder === 'asc' ? valueA - valueB : valueB - valueA;
+      }
+
+      if (typeof valueA === 'string' && typeof valueB === 'string') {
+        return sortOrder === 'asc'
+          ? valueA.localeCompare(valueB)
+          : valueB.localeCompare(valueA);
+      }
+
+      return 0;
     });
 
     setSortedData(sorted);
@@ -71,7 +61,24 @@ const Campaigns: FC = () => {
   return (
     <DefaultPage title={`Campaigns of selected profile ${id}`}>
       <div className="campaigns__btn-sort--wrap">
-        <Button onClick={handleSort}>Sort by Date</Button>
+        <Button
+          className="campaigns__btn-sort"
+          onClick={() => handleSort('date')}
+        >
+          Sort by Date
+        </Button>
+        <Button
+          className="campaigns__btn-sort"
+          onClick={() => handleSort('cost')}
+        >
+          Sort by Cost
+        </Button>
+        <Button
+          className="campaigns__btn-sort"
+          onClick={() => handleSort('clicks')}
+        >
+          Sort by Clicks
+        </Button>
       </div>
       <TableCustom
         columns={columnsCampaign}
