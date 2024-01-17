e09d765f198cbc0c0ac653e81fec70a1260d5bfb
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-01-17 21:20:38 +0200

    updated ui

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Footer.scss';
 
 const Footer: FC = () => {
   return (
     <footer className="footer footer-add mt-auto py-3 bg-light">
-      <div className="container">
+      <div
+        className="container"
+        style={{ display: 'flex', justifyContent: 'center' }}
+      >
         <span className="text-muted">© 2024 aagency. All rights reserved.</span>
       </div>
     </footer>
